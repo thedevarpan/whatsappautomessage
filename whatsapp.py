025e5ab0ac8b84fa2fd4d1456f8e4c85345e9686
Arpan Bera <116783635+ItsCyeberArpan@users.noreply.github.com>
2022-12-09 11:35:29 +0530

    Update whatsapp.py
    Add some visualization

--- whatsapp.py
+++ whatsapp.py
@@ -1,9 +1,14 @@
 import pyautogui as pg
 import time
+
+#select number of message
 print("How many message you want to send: ")
 number_of_message=int(input())
+
+#enter the message
 print("Write down your message: ")
 your_text=input()
+
 time.sleep(5)
 for i in range(number_of_message):
                pg.write(your_text)
